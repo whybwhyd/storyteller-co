c35bce4960fe066d10be124bad8e3a2e7e25062c
YJ Baek <yjbaek29@gmail.com>
2023-07-17 20:32:25 +0900

    Doc: Pagination change

--- src/shared/Router.js
+++ src/shared/Router.js
@@ -1,19 +1,19 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import JoinPage from '../pages/JoinPage'
 import MainPage from '../pages/MainPage'
-import LoginPage from '../pages/LoginPage'
 import DetailsPage from '../pages/DetailsPage'
 import AdminPage from '../pages/AdminPage'
+import SearchPage from '../pages/SearchPage'
+import AboutPage from '../pages/AboutPage'
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<MainPage />} />
-        <Route path='/login' element={<LoginPage />} />
-        <Route path='/join' element={<JoinPage />} />
+        <Route path='/search' element={<SearchPage />} />
         <Route path='/details/:id' element={<DetailsPage />} />
+        <Route path='/about' element={<AboutPage />} />
         <Route path='/admin' element={<AdminPage />} />
       </Routes>
     </BrowserRouter>
