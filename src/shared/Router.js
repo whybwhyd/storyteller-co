c5d79f3518e7483eff531e36fbacda8a5efa600e
leeseojin <dltjs221@naver.com>
2023-07-18 15:58:22 +0900

    FeatAboutpage

--- src/shared/Router.js
+++ src/shared/Router.js
@@ -4,6 +4,7 @@ import JoinPage from '../pages/JoinPage'
 import MainPage from '../pages/MainPage'
 import LoginPage from '../pages/LoginPage'
 import DetailsPage from '../pages/DetailsPage'
+import AboutPage from '../pages/AboutPage'
 
 const Router = () => {
   return (
@@ -13,6 +14,7 @@ const Router = () => {
         <Route path='/login' element={<LoginPage />} />
         <Route path='/join' element={<JoinPage />} />
         <Route path='/details/:id' element={<DetailsPage />} />
+        <Route path='/AboutPage' element={<AboutPage />} />
       </Routes>
     </BrowserRouter>
   )
