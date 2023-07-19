563c4bf319f1cf2595b1393d8cd1fe5e7c985a88
yena <jntantmsemt@naver.com>
2023-07-20 01:25:43 +0900

    FEAT/useParams로 Detailpage 페이지 구성 및 파이어베이스 데이터 불러옴

--- src/pages/DetailsPage.jsx
+++ src/pages/DetailsPage.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import * as St from '../style/StDetailStyled'
 import { useParams } from 'react-router-dom'
 import { collection, getDocs, query } from 'firebase/firestore'
@@ -8,42 +8,52 @@ import { DetailButton, UpButton } from '../components/Button'
 
 const DetailsPage = () => {
   const [liked, setLiked] = useState(false)
+  const [infos, setInfos] = useState([])
   const { title } = useParams()
 
   const handleLike = () => {
     setLiked(!liked)
   }
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const q = query(collection(db, 'infos'))
+      const querySnapshot = await getDocs(q)
+      const initialInfos = []
+      querySnapshot.forEach((doc) => {
+        initialInfos.push({ id: doc.id, ...doc.data() })
+      })
+      setInfos(initialInfos)
+    }
+
+    fetchData()
+  }, [])
+
+  if (!infos) {
+    return <div>Loading...</div>
+  }
+
   return (
     <div id='1'>
       <St.Grid>
-        <St.DramaImg src={Drama1} alt='알고있지만 드라마 사진' />
-        <h4> editor-name</h4>
-        {/* <St.Context>Title<br/><br/>book-introduction-context</St.Context> */}
-        <St.Context>
-          <h2>알고있지만</h2>
-          <div>
-            사랑은 못 믿어도 연애는 하고 싶은 여자, 나비 연애는 성가셔도 썸은 타고 싶은 남자, 재언
-            <br />
-            결코 평범하지 않은 청춘들의 하이퍼리얼리즘 로맨스.
-            <br />
-            <br />
-            알고 있지만, 그럼에도 불구하고.
-          </div>
-        </St.Context>
-        <St.Context>
-          {/* <div>author-context</div> */}
-          <h2>제작정보</h2>
-          <div>
-            <h4>제작사</h4>
-            비욘드제이, 스튜디오N, JTBC스튜디오 제작진
-            <h4>제작</h4>
-            정아름, 권미경, 박성은
-            <h4>연출</h4>
-            김가람, 장지연 PD 김보름, 박우람
-          </div>
-        </St.Context>
-        <St.YoutubeContext>youtube-privew</St.YoutubeContext>
-        <DetailButton handleLike={handleLike} liked={liked} />
+        {infos.map((info) => {
+          return (
+            <div>
+              <St.DramaImg src={Drama1} alt='알고있지만 드라마 사진' />
+              <h4> {info.createdBy}</h4>
+              <St.Context>
+                <h2>{info.title}</h2>
+                <div>{info.body}</div>
+              </St.Context>
+              <St.Context>
+                <h2>제작정보</h2>
+                <div>{info.director}</div>
+              </St.Context>
+              <St.YoutubeContext>youtube-privew</St.YoutubeContext>
+              <DetailButton handleLike={handleLike} liked={liked} />
+            </div>
+          )
+        })}
       </St.Grid>
       <UpButton />
     </div>
