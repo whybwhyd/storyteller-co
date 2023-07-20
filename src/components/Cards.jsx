5082fd344b0003c958ec82daeb47ce98d616c4bc
YJ Baek <yjbaek29@gmail.com>
2023-07-20 09:42:55 +0900

    Feat: filter funcion with Search

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,30 +1,41 @@
 import React from 'react'
 import { styled } from 'styled-components'
+import { useNavigate } from 'react-router-dom'
+import { useQuery } from 'react-query'
+import { getPosts } from '../axios/api'
 
 function Cards() {
+  const navigate = useNavigate()
+
+  // useQuery로 데이터 전체 불러오기
+  const { isLoading, isError, data } = useQuery('posts', getPosts)
+  // console.log('See if data is coming to Cards', data)
+
+  if (isLoading) {
+    return <h1>아직 로딩 중</h1>
+  }
+  if (isError) {
+    return <h1>오류 발생</h1>
+  }
+
+  const navigateToDetailHandler = (id) => {
+    // navigate(`/details/:id`)
+    navigate(`/details/${id}`)
+  }
+
   return (
     <>
       <StCardsList>
-        <StCards>
-          <div>img</div>
-          <div>title</div>
-          <div>text</div>
-        </StCards>
-        <StCards>
-          <div>img</div>
-          <div>title</div>
-          <div>text</div>
-        </StCards>
-        <StCards>
-          <div>img</div>
-          <div>title</div>
-          <div>text</div>
-        </StCards>
-        <StCards>
-          <div>img</div>
-          <div>title</div>
-          <div>text</div>
-        </StCards>
+        {data.map((item, index) => (
+          <StCards key={index} onClick={() => navigateToDetailHandler(item.id)}>
+            <div>{item.img}</div>
+            <div>{item.category}</div>
+            <div>{item.title}</div>
+            <div>{item.body}</div>
+            <div>{item.director}</div>
+            <div>{item.like.toString()}</div>
+          </StCards>
+        ))}
       </StCardsList>
     </>
   )
--- src/pages/SearchPage.jsx
+++ src/pages/SearchPage.jsx
@@ -1,28 +1,71 @@
 import React, { useState } from 'react'
 import Cards from '../components/Cards'
+import { useQuery } from 'react-query'
+import { getPosts } from '../axios/api'
+
+import * as St from '../style/StSearchStyled'
 
 const SearchPage = () => {
+  // useQuery로 데이터 전체 불러오기
+  const { data } = useQuery('posts', getPosts)
+
   // state to keep track of the selected category
   const [selectedCategory, setSelectedCategory] = useState(null)
+  const [categoryResults, setCategoryResults] = useState(null)
 
   // state to keep track of the searched result
-  // const [search, setSearch] = useState(null)
+  const [searchResults, setSearchResults] = useState(null)
+
+  // state to keep track of the searched result
+  const [noResults, setNoResults] = useState(null)
 
   // function to handle category button clicks
   const openCategoryHandler = (category) => {
+    // Results In ..."~" state 변경
     setSelectedCategory(category)
+    // Clear the search results when a category is selected
+    setSearchResults(null) // Clear search results
+
+    // Reset the noResults state if it was set
+    setNoResults(false)
+
+    // Filter the data based on the selected category
+    const filteredCategoryResults = data.filter((post) => post.category === category)
+    setCategoryResults(filteredCategoryResults)
+
+    console.log(selectedCategory)
+    console.log(categoryResults) // 필터링 된 정보는 잘받아옹는데!
+    // console.log(categoryResults)
+  }
+
+  // function to handle search button clicks
+  // *** 조건문으로 noReults까지 구현해보기
+  const openSearchResultsHandler = (title) => {
+    setSelectedCategory(null) // 기존 선택한 카테고리 정보 초기화
+
+    // 검색한 값을 포함하는 데이터만 필터링하여 결과로 저장
+    const filteredSearchResults = data.filter((post) => post.title.includes(title))
+
+    // ***확인필요***
+    if (filteredSearchResults.length > 0) {
+      setSearchResults(filteredSearchResults) // 결과가 있을 경우 결과로 저장
+      setNoResults(false) // Reset the noResults state if it was set
+    } else {
+      setSearchResults(null) // 결과가 없을 경우 검색 결과를 초기화
+      setNoResults(true) // Show the noResults message
+    }
   }
 
   return (
     <>
       <h1>SearchPage</h1>
       <input type='text' placeholder='Search Here' />
-      <button>Search</button>
+      <St.SearchBtn onClick={() => openSearchResultsHandler('title')}>Search</St.SearchBtn>
       <div>
-        <button onClick={() => openCategoryHandler('Category 1')}>Category 1</button>
-        <button onClick={() => openCategoryHandler('Category 2')}>Category 2</button>
-        <button onClick={() => openCategoryHandler('Category 3')}>Category 3</button>
-        <button onClick={() => openCategoryHandler('Category 4')}>Category 4</button>
+        <St.CategoryBtn onClick={() => openCategoryHandler('category1')}>Category 1</St.CategoryBtn>
+        <St.CategoryBtn onClick={() => openCategoryHandler('category2')}>Category 2</St.CategoryBtn>
+        <St.CategoryBtn onClick={() => openCategoryHandler('category3')}>Category 3</St.CategoryBtn>
+        <St.CategoryBtn onClick={() => openCategoryHandler('category4')}>Category 4</St.CategoryBtn>
       </div>
 
       {/* Show the selected category's card list */}
@@ -30,20 +73,26 @@ const SearchPage = () => {
         <div>
           <h2>Results In ... {selectedCategory}</h2>
           <div>카테고리별 카드리스트</div>
+          <Cards data={categoryResults} />
         </div>
       )}
-
+      {searchResults && (
+        <div>
+          <h2>Results For ...</h2>
+          <div>검색값 카드리스트</div>
+          <Cards data={searchResults}></Cards>
+        </div>
+      )}
       <div>
-        <h2>Results For ...</h2>
-        <div>검색값 카드리스트</div>
+        <h2>Now Trending</h2>
+        <Cards data={data} />
       </div>
-      <div>
-        <h2>No Results...</h2>
-        <div>검색값이 없습니다</div>
-      </div>
-      <div>
-        <Cards></Cards>
-      </div>
+      {noResults && (
+        <div>
+          <h2>No Results...</h2>
+          <div>검색값이 없습니다</div>
+        </div>
+      )}
     </>
   )
 }
