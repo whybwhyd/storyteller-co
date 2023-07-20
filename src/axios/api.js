01a0536841b3a2890b3798d9733831e17441dca5
yena <jntantmsemt@naver.com>
2023-07-20 11:17:38 +0900

    Feat/ id와 img를 파이어베이스에 들어가게끔 기능 수정

--- src/axios/api.js
+++ src/axios/api.js
@@ -20,24 +20,17 @@ const getStorys = async () => {
     return initialStorys
 }
 
-export { getStorys }
+const getPosts = async () => {
+  try {
+    const postsCollection = collection(db, 'posts') // 'collection' 함수는 'collection(db, '컬렉션_이름') 형태로 사용
+    const postsSnapshot = await getDocs(postsCollection)
+    const posts = postsSnapshot.docs.map((doc) => doc.data())
+    return posts
+  } catch (error) {
+    console.error('게시물을 불러오는 중 오류 발생:', error)
+    throw error
+  }
+}
 
-// import axios from 'axios'
-// import { db } from '../firebase'
-// import { collection, getDocs } from 'firebase/firestore'
-
-
-// const getPosts = async () => {
-//   try {
-//     const postsCollection = collection(db, 'posts') // 'collection' 함수는 'collection(db, '컬렉션_이름') 형태로 사용
-//     const postsSnapshot = await getDocs(postsCollection)
-//     const posts = postsSnapshot.docs.map((doc) => doc.data())
-//     return posts
-//   } catch (error) {
-//     console.error('게시물을 불러오는 중 오류 발생:', error)
-//     throw error
-//   }
-// }
-
-// export { getPosts }
+export { getStorys, getPosts}
 
