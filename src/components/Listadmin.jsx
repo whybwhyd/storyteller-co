d580985fabbf8372a519de9887ad1bd41a900067
leeseojin <dltjs221@naver.com>
2023-07-20 21:01:45 +0900

    FeatLogout

---
+++ src/components/Listadmin.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Listadmin() {
+  return (
+    <>
+    </>
+
+  )
+}
+
+export default Listadmin
--- src/pages/AdminPage.jsx
+++ src/pages/AdminPage.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
-import { signOut } from 'firebase/auth';
 import { auth } from '../firebase';
+import Listadmin from '../components/Listadmin';
 
 const AdminPage = () => {
   const navigate = useNavigate();
@@ -12,15 +12,15 @@ const AdminPage = () => {
       <div>AdminPage</div>
       <button onClick={async () => {
           alert("로그아웃을 하시겠습니까?");
-          await signOut(auth);
+          await auth.signOut();
           navigate('/');
         }}>Admin Logout</button>
-      <Link to='/'>
-        <button>글 목록</button>
-      </Link>
       <Link to='/write'>
         <button>글 작성</button>
       </Link>
+      <div>
+        <Listadmin></Listadmin>
+      </div>
     </>
   )
 }
