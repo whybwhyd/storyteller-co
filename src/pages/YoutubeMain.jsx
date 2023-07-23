b1ca704a2b615be8c74a00cb8d53056993d88204
kim jin woo <wlsdn090909@naver.com>
2023-07-23 21:32:03 +0900

    Design:Card Css 수정

--- src/pages/YoutubeMain.jsx
+++ src/pages/YoutubeMain.jsx
@@ -6,7 +6,7 @@ import { styled } from 'styled-components'
 
 function YoutubeMain() {
   const [playList, setPlayList] = useState([])
-  // 영상관련
+
   const [videoList, setVideoList] = useState([])
 
   useEffect(() => {
@@ -15,18 +15,15 @@ function YoutubeMain() {
         'https://www.googleapis.com/youtube/v3/playlists?part=snippet&channelId=UC1QidArPD4PLF1HBP7HbcOg&maxResults=5&key=AIzaSyAvy048qvDbI3b_F_JE1-QPMdMyDrVD9Io'
       )
       .then((res) => {
-        // console.log('res=>', res)
         setPlayList(res.data.items)
       })
       .catch(() => {})
   }, [])
-  // console.log('playList=>', playList)
 
-  // 유투브 영상 관련
   useEffect(() => {
     axios
       .get(
-        `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=PLYZAHhM04Ewonz9bUbfaFTR_u7Bg5_BP7&maxResults=5&key=AIzaSyAvy048qvDbI3b_F_JE1-QPMdMyDrVD9Io`
+        `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=PLYZAHhM04Ewonz9bUbfaFTR_u7Bg5_BP7&maxResults=4&key=AIzaSyAvy048qvDbI3b_F_JE1-QPMdMyDrVD9Io`
       )
       .then((res) => {
         console.log('res=>', res)
@@ -34,7 +31,6 @@ function YoutubeMain() {
       })
       .catch(() => {})
   }, [])
-  // console.log('videoList=>', videoList)
   let videoID = []
 
   let [videoInfo, setVideoInfo] = useState([])
@@ -42,7 +38,7 @@ function YoutubeMain() {
   useEffect(() => {
     axios
       .get(
-        `https://www.googleapis.com/youtube/v3/videos?part=snippet,statistics&id=vwxKPgn3Mbo&id=VRI3GVZjFrM&id=CCOANmV-Qpg&id=aPAu-ePYfLg&id=stzQyxvSSt0&id=e0_a93-pabk&id=bNoJiT8cQ3Y&id=AapSj0ncwqU&id=v7WYUm2OyKI&id=b_u7pEZ5dUc&maxResults=5&key=AIzaSyAvy048qvDbI3b_F_JE1-QPMdMyDrVD9Io`
+        `https://www.googleapis.com/youtube/v3/videos?part=snippet,statistics&id=vwxKPgn3Mbo&id=VRI3GVZjFrM&id=CCOANmV-Qpg&id=aPAu-ePYfLg&id=stzQyxvSSt0&id=e0_a93-pabk&id=bNoJiT8cQ3Y&id=AapSj0ncwqU&id=v7WYUm2OyKI&id=b_u7pEZ5dUc&maxResults=4&key=AIzaSyAvy048qvDbI3b_F_JE1-QPMdMyDrVD9Io`
       )
       .then((res) => {
         console.log(res)
@@ -54,26 +50,9 @@ function YoutubeMain() {
 
   return (
     <div>
-      {/* <div>
-        <StYoutubeContainer>
-          {playList &&
-            playList.map((i, idx) => {
-              return (
-                <StYoutubePlayListDiv key={idx}>
-                  <StImgDiv src={i.snippet.thumbnails.high['url']} alt='' />
-                  <Link to={'/playlist/' + i.id}>
-                    <div>{i.snippet.localized['title']}</div>
-                  </Link>
-                  <p>{i.snippet.localized['description']}</p>
-                </StYoutubePlayListDiv>
-              )
-            })}
-        </StYoutubeContainer>
-      </div> */}
       <div>
         <StYoutubeContainer>
           {videoList.map((x) => {
-            // console.log('x.resourceId', x.snippet.resourceId.videoId)
             return (
               <StYoutubePlayListDiv key={x.id}>
                 <a href={`https://www.youtube.com/watch?v=${x.snippet.resourceId.videoId}`}>
@@ -82,7 +61,6 @@ function YoutubeMain() {
 
                 <StVideoTitleDiv>{x.snippet.title}</StVideoTitleDiv>
                 <span>{x.snippet['publishedAt']} </span>
-                {/* <span>조회수 : {Number(x.statistics.viewCount).toLocaleString('en-US')} 회</span> */}
               </StYoutubePlayListDiv>
             )
           })}
@@ -96,11 +74,14 @@ export default YoutubeMain
 
 const StYoutubeContainer = styled.div`
   display: flex;
+  justify-content: center;
   align-items: center;
+  margin-top: 30px;
+  margin-bottom: 30px;
 `
 
 const StYoutubePlayListDiv = styled.div`
-  height: 150px;
+  /* height: 150px;
   width: 200px;
 
   margin: 20px;
@@ -108,12 +89,26 @@ const StYoutubePlayListDiv = styled.div`
   object-fit: cover;
 
   border-radius: 5px 5px 5px 5px;
-  box-shadow: 0 10px 20px rgba(0, 0, 0, 0.15), 0 5px 5px rgba(0, 0, 0, 0.2);
+  box-shadow: 0 10px 20px rgba(0, 0, 0, 0.15), 0 5px 5px rgba(0, 0, 0, 0.2); */
+
+  background-color: #e9e6d8;
+  border-radius: 5px;
+  color: #153e4b;
+  width: 30vh;
+  min-height: 20vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding: 10px;
+  margin-right: 15px;
+  cursor: pointer;
 `
 
 const StImgDiv = styled.img`
-  width: 200px;
-  height: 100px;
+  width: 30vh;
+  height: 20vh;
+  object-fit: cover;
+  object-position: center center;
 `
 
 const StVideoTitleDiv = styled.div`
